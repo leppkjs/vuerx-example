80fc06dbf04f69df620104543ef76dd8c4619acf
leppkjs <leppk@naver.com>
2018-10-14 22:01:30 +0900

    add rxjs module

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
+import VueRx from 'vue-rx';
 import App from './App.vue';
 import router from './router';
+import Rx from 'rxjs/Rx';
 Vue.config.productionTip = false;
+Vue.use(VueRx, Rx);
 new Vue({
     router,
     render: (h) => h(App),
